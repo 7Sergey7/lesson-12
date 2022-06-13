0e1a460b369561093e0fbdf36927fe5fcfa7e59a
7Sergey7 <stepanyansergey7@gmail.com>
2022-06-13 23:42:22 +0400

    Initial Commit

---
+++ lesson 12/main.swift
@@ -0,0 +1,109 @@
+//
+//  main.swift
+//  lesson 12
+//
+//  Created by Seroj on 28.05.22.
+//
+
+import Foundation
+
+//let numberArr = [34, 4, 12, 455]
+
+//let numberDic: [Int : String] = [0 : "Zero",
+//                                 1: "One",
+//                                 2: "Two",
+//                                 3: "Three",
+//                                 4: "Four",
+//                                 5: "Five",
+//                                 6: "Six",
+//                                 7: "Seven",
+//                                 8: "Eight",
+//                                 9: "Nine"]
+
+//var number = 457
+//var resultTextNumber = ""
+//
+//while number != 0 {
+//    let numberItem = number % 10
+//    number /= 10
+//    print(numberItem)
+//    resultTextNumber = numberDic[numberItem]! + resultTextNumber
+//}
+//print(resultTextNumber)
+
+////
+//let newStringArr = numberArr.map( { (arrItem: Int) -> String in
+//    var number = arrItem
+//    var resultTextNumber = ""
+//
+//    while number != 0 {
+//        let numberItem = number % 10
+//        number /= 10
+//        resultTextNumber = numberDic[numberItem]! + resultTextNumber
+//    }
+//    return resultTextNumber
+//
+//} )
+//
+//print(newStringArr)
+
+
+
+//let stringArr = numberArr.map({(arrItem: Int) -> String in return "\(arrItem)"})
+//print(stringArr)
+
+//func intToStrinh(a: Int) -> String {
+//    return "\(a)"
+//}
+//
+//func map(arr: [Int], helper: (Int) -> String) -> [String] {
+//    var result: [String] = []
+//    for item in arr {
+//        let resultStr = helper(item)
+//        result.append(resultStr)
+//    }
+//    return result
+//}
+//let newArr = map(arr: numberArr, helper: intToStrinh)
+//print(newArr)
+//
+//let stringArr = numberArr.map({(arrItem: Int) -> String in
+//    let stringNumber = "\(arrItem)" + "$"
+//    return stringNumber
+//})
+//print(stringArr)
+
+
+//let array = [3, 4, 6, 7]
+
+//var array12: [Int] = []
+//
+//for i in array {
+//    array12.append(i * i)
+//}
+//print(array12)
+
+//let array1 = array.map{ i in i * i}
+//
+//print(array1)
+
+
+//func someFunc() -> () -> Int {
+//    var a = 7
+//
+//    func someTwoFun() -> Int {
+//        a += 1
+//        return a
+//    }
+//
+//    return someTwoFun
+//
+//}
+//let someFuncion = someFunc()
+//
+//let aValue = someFuncion()
+//print(aValue)
+
+//////////////////////////////////////////////////////////////////////////////\\
+
+
